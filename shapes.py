cffa49a4578b6df4842271ec4c9b614744a96715
Franckapik <fanch44@hotmail.com>
2023-08-01 21:04:23 +0200

    fix mortaise long dif

--- shapes.py
+++ shapes.py
@@ -135,7 +135,7 @@ def add_cadre_tenon(difprops):
 
     vertsMortaisesInt = []
 
-    for k in range(1, round(N * longueur_diffuseur)):
+    for k in range(1, round(N)):
             vertsMortaisesInt += [
                 (bord_cadre - offset / 2, startup + rang * k + epaisseur / 2, 0),
                 (
