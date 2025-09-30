7563cfe10dd6e9d732de9b024e48e30666f2e954
Franckapik <fanch44@hotmail.com>
2025-09-30 09:29:23 +0200

    Version 1.1.0

--- version.py
+++ version.py
@@ -1,8 +1,8 @@
 # Version de l'addon Diffuseur CAM
-__version__ = "1.0.2"
+__version__ = "1.1.0"
 
 # Pour le développement, vous pouvez aussi ajouter:
-__version_info__ = (1, 0, 2)
+__version_info__ = (1, 1, 0)
 
 # Informations sur la release
 GITHUB_REPO = "Franckapik/diffuseurcam"
