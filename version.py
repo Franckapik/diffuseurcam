50aa9fea829481fc1dc5e43fd8fe77dbb1bfae23
Fanch <fanch44@hotmail.com>
2026-02-13 22:55:45 +0100

    Version 2.3.0

--- version.py
+++ version.py
@@ -1,8 +1,8 @@
 # Version de l'addon Diffuseur CAM
-__version__ = "2.2.0"
+__version__ = "2.3.0"
 
 # Pour le développement, vous pouvez aussi ajouter:
-__version_info__ = (2, 2, 0)
+__version_info__ = (2, 3, 0)
 
 # Informations sur la release
 GITHUB_REPO = "Franckapik/diffuseurcam"
